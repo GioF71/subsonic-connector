96fc7e1cf025fa341bb57e8906d31eef29a3232a
GioF_71 <giovanni.fulco@gmail.com>
2023-01-09 18:17:22 +0100

    initial stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# Subsonic Connector
+
+## Instructions
+
+Create your own `.env` file. Use `.sample.env` as a reference for the format
+
+## Usage
+
+TODO. See `subsonic_connector/test-cn.py`.
--- subsonic_connector/test-cn.py
+++ subsonic_connector/test-cn.py
@@ -9,29 +9,6 @@ SERVER_PORT = config('SERVER_PORT')
 USERNAME = config('USERNAME')
 PASSWORD = config('PASSWORD')
 
-#print('Connecting to SERVER {} at port {}'.format(SERVER_URL, SERVER_PORT))
-#print(f'Connecting to SERVER {SERVER_URL} at port {SERVER_PORT}')
-
-# We pass in the base url, the username, password, and port number
-# Be sure to use https:// if this is an ssl connection!
-#conn = libsonic.Connection(SERVER_URL, USERNAME, PASSWORD, port=SERVER_PORT)
-
-#print(conn._getBaseQdict())
-
-# Let's get 2 completely random songs
-#songs = conn.getRandomSongs(size=2)
-#print(type(songs))
-# We'll just pretty print the results we got to the terminal
-#pprint(songs)
-
-#randomSongs = songs['randomSongs']
-#print(type(randomSongs))
-
-#for song in songs['randomSongs']['song']:
-    #print(type(song))
-    #print("Song id is {}".format(song["id"]))
-
-
 ssc = connector.Connector(
     baseUrl = SERVER_URL, 
     port = SERVER_PORT, 
@@ -39,21 +16,22 @@ ssc = connector.Connector(
     password = PASSWORD,
     appName="navibridge")
 
-songs = ssc.getRandomSongs(size = 3)
-#print(type(songs))
-# We'll just pretty print the results we got to the terminal
-#pprint(songs)
+# Newest albums (two albums expected)
+newest_albums = ssc.getNewestAlbumList(size = 2)
+pprint(newest_albums)
 
+# Random album (just one)
+random_album = ssc.getRandomAlbumList(size = 1)
+pprint(random_album)
+
+# Random songs (three expected)
+songs = ssc.getRandomSongs(size = 3)
+# full dump with pprint
+#pprint(songs)
 for song in songs['randomSongs']['song']:
-    #print(type(song))
     print("Song id: {}".format(song["id"]))
     streamable_url = ssc.buildSongUrl(song["id"])
     cover_url = ssc.buildCoverArtUrl(song["id"])
     print(" -> Stream = [" + streamable_url + "]")
     print(" -> Cover  = [" + cover_url + "]")
 
-newest_albums = ssc.getNewestAlbumList(size = 2)
-pprint(newest_albums)
-
-random_album = ssc.getRandomAlbumList(size = 1)
-pprint(random_album)
