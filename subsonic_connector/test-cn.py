eacf9f6f41c7a6b877afeead1fc78d83c4c0e425
GioF71 <giovanni.fulco@gmail.com>
2023-01-10 16:13:36 +0100

    test modularity

--- subsonic_connector/test-cn.py
+++ subsonic_connector/test-cn.py
@@ -55,74 +55,77 @@ def ewf(ssc):
             ewfSong.getDiscNumber(),
             ewfSong.getTrack()))
         sc += 1
-    
 
+def newest_albums(ssc):
+    # Newest albums (two albums expected)
+    newest_album_list : list[Album] = ssc.getNewestAlbumList(size = 2).getAlbums()
+    for album in newest_album_list:
+        print("Album {} = [{}] Genre [{}]".format(
+            album.getId(), 
+            album.getTitle(),
+            album.getGenre()))
 
-# Newest albums (two albums expected)
-newest_album_list : list[Album] = ssc.getNewestAlbumList(size = 2).getAlbums()
-for album in newest_album_list:
-    print("Album {} = [{}] Genre [{}]".format(
-        album.getId(), 
-        album.getTitle(),
-        album.getGenre()))
+def random_albums(ssc):
+    # Random album (just one)
+    random_album_list : list[Album] = ssc.getRandomAlbumList(size = 1000).getAlbums()
+    for album in random_album_list:
+        print("Album {} = [{}] Genre [{}]".format(
+            album.getId(), 
+            album.getTitle(),
+            album.getGenre()))
 
-# Random album (just one)
-random_album_list : list[Album] = ssc.getRandomAlbumList(size = 1000).getAlbums()
-for album in random_album_list:
-    print("Album {} = [{}] Genre [{}]".format(
-        album.getId(), 
-        album.getTitle(),
-        album.getGenre()))
+def random_songs(ssr):
+    random_songs : list = ssc.getRandomSongs(size = 3).getSongs()
+    for current in random_songs:
+        current_song : Song = Song(current)
+        song = current_song.getData()
+        print("Song [{}] A:[{}] G:[{}] D:[{}] T:[{}]".format(
+            current_song.getTitle(), 
+            current_song.getAlbum(),
+            current_song.getGenre(),
+            current_song.getDiscNumber(), 
+            current_song.getTrack()))
+        id = current_song.getId()
+        streamable_url = ssc.buildSongUrl(id)
+        cover_url = ssc.buildCoverArtUrl(id)
+        #print(" -> Stream = [" + streamable_url + "]")
+        #print(" -> Cover  = [" + cover_url + "]")
 
-random_songs : list = ssc.getRandomSongs(size = 3).getSongs()
-for current in random_songs:
-    current_song : Song = Song(current)
-    song = current_song.getData()
-    print("Song [{}] A:[{}] G:[{}] D:[{}] T:[{}]".format(
-        current_song.getTitle(), 
-        current_song.getAlbum(),
-        current_song.getGenre(),
-        current_song.getDiscNumber(), 
-        current_song.getTrack()))
-    id = current_song.getId()
-    streamable_url = ssc.buildSongUrl(id)
-    cover_url = ssc.buildCoverArtUrl(id)
-    #print(" -> Stream = [" + streamable_url + "]")
-    #print(" -> Cover  = [" + cover_url + "]")
+def artists(ssc):
+    max_per_initial : int = 3
+    artists : Artists = ssc.getArtists()
+    all_artists_initials : list[ArtistsInitial] = artists.getArtistListInitials()
+    for current_initial in all_artists_initials:
+        cnt : int = 0
+        c : ArtistListItem
+        for c in current_initial.getArtistListItems():
+            cnt += 1
+            if cnt > max_per_initial:
+                break
+            print("Count [{}] Initial [{}] Artist: {}".format(
+                cnt,
+                current_initial.getName(),
+                c.getName()))
+            artist : Artist = ssc.getArtist(c.getId())
+            first_album_name : str = None
+            first_album_cover_art : str = None
+            album_list = artist.getAlbumList()
+            if len(album_list) > 0:
+                first_album = album_list[0]
+                selected_album : Album = Album(first_album)
+                first_album_name = selected_album.getTitle()
+                first_album_cover_art = selected_album.getCoverArt()
+                if first_album_cover_art:
+                    first_album_cover_art = hashlib.md5(first_album_cover_art.encode('utf-8')).hexdigest()
+            print("Artist Initial[{}] N:[{}] AC:[{}] First:[{}] HashedCover:[{}]".format(
+                current_initial.getName(), 
+                c.getName(), 
+                c.getAlbumCount(),
+                first_album_name,
+                first_album_cover_art))
 
-#####
-# EWF
+random_albums(ssc)
+newest_albums(ssc)
+random_songs(ssc)
 ewf(ssc)
-
-#####
-max_per_initial : int = 3
-artists : Artists = ssc.getArtists()
-all_artists_initials : list[ArtistsInitial] = artists.getArtistListInitials()
-for current_initial in all_artists_initials:
-    cnt : int = 0
-    c : ArtistListItem
-    for c in current_initial.getArtistListItems():
-        cnt += 1
-        if cnt > max_per_initial:
-            break
-        print("Count [{}] Initial [{}] Artist: {}".format(
-            cnt,
-            current_initial.getName(),
-            c.getName()))
-        artist : Artist = ssc.getArtist(c.getId())
-        first_album_name : str = None
-        first_album_cover_art : str = None
-        album_list = artist.getAlbumList()
-        if len(album_list) > 0:
-            first_album = album_list[0]
-            selected_album : Album = Album(first_album)
-            first_album_name = selected_album.getTitle()
-            first_album_cover_art = selected_album.getCoverArt()
-            if first_album_cover_art:
-                first_album_cover_art = hashlib.md5(first_album_cover_art.encode('utf-8')).hexdigest()
-        print("Artist Initial[{}] N:[{}] AC:[{}] First:[{}] HashedCover:[{}]".format(
-            current_initial.getName(), 
-            c.getName(), 
-            c.getAlbumCount(),
-            first_album_name,
-            first_album_cover_art))
+artists(ssc)
