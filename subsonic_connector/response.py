02398315878950b22a8afa57234b2d910387e7d1
Giovanni Fulco <giovanni.fulco@gmail.com>
2023-01-30 16:30:38 +0100

    [Feature] Create Response from dict #20 (#21)

--- subsonic_connector/response.py
+++ subsonic_connector/response.py
@@ -2,11 +2,11 @@ from .item import Item
 
 class Response:
     
-    def __init__(self, item : Item):
-        self.item = item
-
-    def getItem(self):
-        return self.item
+    def __init__(self, data : dict):
+        self.__item : Item = Item(data)
 
     def getStatus(self) -> str | None:
-        return self.getItem().getByName("status")
+        return self.__item.getByName("status")
+
+    def getVersion(self) -> str | None:
+        return self.__item.getByName("version")
