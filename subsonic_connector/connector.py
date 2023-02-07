2e88569b918aecdaeccc5115d4cafb2b1a250a35
Giovanni Fulco <giovanni.fulco@gmail.com>
2023-02-07 14:33:26 +0100

    [Feature] Generic class for responses #28

--- subsonic_connector/connector.py
+++ subsonic_connector/connector.py
@@ -9,6 +9,7 @@ from .random_songs import RandomSongs
 from .search_result import SearchResult
 from .artist_cover import ArtistCover
 from .genres import Genres
+from .response import Response
 
 from .configuration import Configuration
 
@@ -24,11 +25,13 @@ class Connector:
             musicFolderId = musicFolderId, 
             ifModifiedSince = ifModifiedSince)
 
-    def getArtists(self) -> Artists:
-        return Artists(self.__connect().getArtists())
+    def getArtists(self) -> Response[Artists]:
+        data = self.__connect().getArtists()
+        return Response(data, Artists(data) if data else None)
 
-    def getGenres(self) -> Genres:
-        return Genres(self.__connect().getGenres())
+    def getGenres(self) -> Response[Genres]:
+        data = self.__connect().getGenres()
+        return Response(data, Genres(data) if data else None)
 
     def getCoverArtForGenre(self,
             genre : str,
@@ -40,7 +43,6 @@ class Connector:
             raise Exception("No songs for genre [{}]"
                 .format(genre))
         songList = Item(songs).getList(["songsByGenre", "song"])
-        #songList : list = songs["songsByGenre"]["song"]
         if not songList: return None
         coverArt : str = ""
         for current in songList:
@@ -51,35 +53,35 @@ class Connector:
             size = 10, 
             genre = None, 
             fromYear = None, toYear = None, 
-            musicFolderId = None) -> RandomSongs:
-        return RandomSongs(self.__connect().getRandomSongs(
+            musicFolderId = None) -> Response[RandomSongs]:
+        data : dict = self.__connect().getRandomSongs(
             size = size, 
             genre = genre, 
             fromYear = fromYear, toYear = toYear, 
-            musicFolderId = musicFolderId))
+            musicFolderId = musicFolderId)
+        return Response(data, RandomSongs(data) if data else None)
 
     def getAlbumList(self, 
             ltype, 
             size = 10, offset = 0, 
             fromYear = None, toYear = None, 
-            genre = None, musicFolderId = None) -> AlbumList:
-        return AlbumList(self.__connect().getAlbumList(
+            genre = None, musicFolderId = None) -> Response[AlbumList]:
+        data : dict = self.__connect().getAlbumList(
             ltype = ltype, 
             size = size, offset = offset, 
             fromYear = fromYear, toYear = toYear,
-            genre = genre, musicFolderId = musicFolderId))
+            genre = genre, musicFolderId = musicFolderId)
+        return Response(data, AlbumList(data) if data else None)
 
     def getAlbum(self,
-            albumId : str) -> Album | None:
-        album = self.__connect().getAlbum(albumId)
-        return (Album(album) 
-            if album 
-            else None)
+            albumId : str) -> Response[Album]:
+        data = self.__connect().getAlbum(albumId)
+        return Response(data, Album(data) if data else None)
             
     def getNewestAlbumList(self, 
             size = 10, offset = 0, 
             fromYear = None, toYear = None, 
-            genre = None, musicFolderId = None) -> AlbumList:
+            genre = None, musicFolderId = None) -> Response[AlbumList]:
         return self.getAlbumList(
             ltype = "newest", 
             size = size, offset = offset, 
@@ -89,19 +91,23 @@ class Connector:
     def getRandomAlbumList(self, 
             size = 10, offset = 0, 
             fromYear = None, toYear = None, 
-            genre = None, musicFolderId = None) -> AlbumList:
+            genre = None, musicFolderId = None) -> Response[AlbumList]:
         return self.getAlbumList(
             ltype = "random", 
             size = size, offset = offset, 
             fromYear = fromYear, toYear = toYear,
             genre = genre, musicFolderId = musicFolderId)
 
-    def getArtist(self, artist_id : str) -> Artist:
-        return Artist(self.__connect().getArtist(artist_id))
+    def getArtist(self, artist_id : str) -> Response[Artist]:
+        data = self.__connect().getArtist(artist_id)
+        return Response(data, Artist(data) if data else None)
 
-    def getArtistCover(self, artist : Artist) -> ArtistCover | None:
-        first_album : str
-        first_album_cover_art : str
+    def getCoverByArtistId(self, artist_id : str) -> ArtistCover | None:
+        artist_response : Response[Artist] = self.getArtist(artist_id)
+        if not artist_response and not artist_response.getObj(): return None
+        return self.getCoverByArtist(artist_response.getObj())
+
+    def getCoverByArtist(self, artist : Artist) -> ArtistCover | None:
         album_list : list[Album] = artist.getAlbumList()
         current : Album
         for selected_album in album_list:
