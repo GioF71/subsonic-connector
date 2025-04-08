1a74ee90e06236aa1d176fe2431faa141abc8c77
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-04-08 15:39:37 +0200

    Add test

--- tests/test-cn-lms.py
+++ tests/test-cn-lms.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 from pprint import pprint
-import libsonic
 import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+
+sys.path.append(vendor_dir)
+
+import libsonic
 
 base_url : str = os.getenv("SUBSONIC_SERVER_URL")
 port : int = int(os.getenv("SUBSONIC_SERVER_PORT"))
